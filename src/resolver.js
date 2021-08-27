91272dc588a35b069e8b039e7c29b16421339813
Nemecd <samueltau182@gmail.com>
2021-08-27 15:03:33 +0100

    Feature: Add Prisma

--- src/resolver.js
+++ src/resolver.js
@@ -1,45 +1,93 @@
 const { students } =  require('./database.js');
 
+// const resolvers = {
+
+//     Student: {
+//         id: (parent, args, context, info) => parent.id,
+//         email: (parent) => parent.email,
+//         fullName: (parent) => parent.fullName,
+//         dept: (parent) => parent.dept,
+//         enrolled: (parent) => parent.enrolled,
+//       },
+
+//     Query: {
+//       enrollment: (parent, args) => {
+//         return students.filter((student) => student.enrolled)
+//       },
+//       student: (parent, args) => {
+//         return students.find((student) => student.id === Number(args.id))
+//       },
+//     },
+
+//     Mutation: {
+//       registerStudent: (parent, args) => {
+//         students.push({
+//           id: students.length + 1,
+//           email: args.email,
+//           fullName: args.fullName,
+//           dept: args.dept,
+//           enrolled: false,
+//         })
+//         return students[students.length - 1]
+//       },
+//       enroll: (parent, args) => {
+//         const studentToEnroll = students.find((student) => student.id === Number(args.id))
+//         studentToEnroll.enrolled = true
+//         return studentToEnroll
+//       },
+//     },
+
+//   }
+
+
+//   module.exports = {
+//     resolvers,
+//   }
+
+
 const resolvers = {
-
-    Student: {
-        id: (parent, args, context, info) => parent.id,
-        email: (parent) => parent.email,
-        fullName: (parent) => parent.fullName,
-        dept: (parent) => parent.dept,
-        enrolled: (parent) => parent.enrolled,
-      },
-
-    Query: {
-      enrollment: (parent, args) => {
-        return students.filter((student) => student.enrolled)
-      },
-      student: (parent, args) => {
-        return students.find((student) => student.id === Number(args.id))
-      },
+  Student: {
+            id: (parent, args, context, info) => parent.id,
+            email: (parent) => parent.email,
+            fullName: (parent) => parent.fullName,
+            dept: (parent) => parent.dept,
+            enrolled: (parent) => parent.enrolled,
+          },
+    
+  Query: {
+    enrollment: (parent, args) => {
+      return prisma.student.findMany({
+        where: { enrolled: true },
+      });
     },
-
-    Mutation: {
-      registerStudent: (parent, args) => {
-        students.push({
-          id: students.length + 1,
+    students: (parent, args) => {
+      return prisma.student.findMany({});
+    },
+    student: (parent, args) => {
+      return prisma.student.findFirst({
+        where: { id: Number(args.id) },
+      });
+    },
+  },
+  Mutation: {
+    registerStudent: (parent, args) => {
+      return prisma.student.create({
+        data: {
           email: args.email,
           fullName: args.fullName,
-          dept: args.dept,
-          enrolled: false,
-        })
-        return students[students.length - 1]
-      },
-      enroll: (parent, args) => {
-        const studentToEnroll = students.find((student) => student.id === Number(args.id))
-        studentToEnroll.enrolled = true
-        return studentToEnroll
-      },
+        },
+      });
+
     },
-
-  }
-
-
-  module.exports = {
-    resolvers,
-  }
+    enroll: (parent, args) => {
+      return prisma.student.update({
+        where: {
+          id: Number(args.id),
+        },
+        data: {
+          enrolled: true,
+        },
+      });
+    },
+  },
+}
